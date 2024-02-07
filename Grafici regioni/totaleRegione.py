c61b2e6c8a8932310ea701bc24df4053e0d702fc
Federico Zanoni <153846199+Federicozan02@users.noreply.github.com>
2024-02-07 12:09:37 +0100

    Update totaleRegione.py

--- Grafici regioni/totaleRegione.py
+++ Grafici regioni/totaleRegione.py
@@ -1,19 +1,22 @@
-#Raggruppo per regione
-dati_raggruppati = dati.groupby('RegionName')["TotalPositiveCases"].sum().reset_index()
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
-# Calcolare il minimo e il massimo dei dati TotalPositiveCases
-min_value = dati_raggruppati["TotalPositiveCases"].min()
-max_value = dati_raggruppati["TotalPositiveCases"].max()
-# Normalizzare i dati TotalPositiveCases
-dati_raggruppati["TotalPositiveCases_normalized"] = (dati_raggruppati["TotalPositiveCases"] - min_value) / (max_value - min_value)
+file = "regioniPulite.csv"
+df = pd.read_csv(file)
+
+#Raggruppo per regione
+dati_raggruppati = df.groupby('RegionName')["NewPositiveCases"].sum().reset_index()
+
+#Importiamoli in un csv per la distribuzione geografica su excel
+dati_raggruppati.to_csv("TotaleRegione.csv", index = False)
 
 #Ordiniamoli per valore
-dati_raggruppati = dati_raggruppati.sort_values(by="TotalPositiveCases_normalized", ascending=False)
-
+dati_raggruppati = dati_raggruppati.sort_values(by="NewPositiveCases", ascending=False)
 
 #Creiamo il grafico
-sns.barplot(x="RegionName", y="TotalPositiveCases_normalized", data=dati_raggruppati, color="#82c89f")
-plt.title('Casi positivi registrati (Normalizzati)')
+sns.barplot(x="RegionName", y="NewPositiveCases", data=dati_raggruppati, color="#82c89f")
+plt.title('Casi positivi registrati')
 plt.xlabel('Casi positivi')
 plt.ylabel('Totale')
 plt.xticks(rotation=45, ha = "right")
